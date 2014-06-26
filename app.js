54adaa8e1b90159eae0b3177a576443b3ab4c3b5
darinrogers <darinrogers@hotmail.com>
2014-06-25 17:57:23 -0700

    Added procfile to run on Heroku

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.post('/configuration/save-robocoin', configuration.saveRobocoin);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
 
+    console.log('Express server listening on port ' + app.get('port'));
     winston.log('Express server listening on port ' + app.get('port'));
 
     var jobs = require('./periodicJobs');
