185528e50c81ba93f58e7e8b1506432ac17ca3a8
darinrogers <darinrogers@hotmail.com>
2014-07-16 16:25:34 -0700

    Removed logging to filesystem

--- logConfig.js
+++ logConfig.js
@@ -3,5 +3,4 @@
 var winston = require('winston');
 var PostgresLogger = require('./data_mappers/PostgresLogger');
 
-winston.add(winston.transports.File, { filename: './its.log' });
 winston.add(PostgresLogger);
