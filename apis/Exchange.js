00e4e3f429dee43836cc888e2905ff1b2fe9a9a2
darinrogers <darinrogers@hotmail.com>
2014-07-24 18:31:28 -0700

    Logging exchange config error instead of throwing exception

--- apis/Exchange.js
+++ apis/Exchange.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var winston = require('winston');
+
 module.exports = {
     get: function (config) {
 
         if (!config.exchangeClass) {
-            throw 'Exchange not configured!';
+            winston.error('Exchange not configured!');
         }
 
         return require('./exchanges/' + config.exchangeClass)
